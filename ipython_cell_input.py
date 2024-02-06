ef06a5450e7e6104e7bafc43483352d355f938fc
Josh <hemangjoshi37a@gmail.com>
2024-02-06 20:31:37 +0000

    Delete ipython_cell_input.py

--- ipython_cell_input.py
+++
@@ -1,9 +0,0 @@
-
-quote_list = []
-
-for i in n50df['tradingsymbol']:
-    print(i)
-    myquote = kite.quote([exchange_type+':'+i])
-    quote_list.append(myquote)
-quote_list
-#     print(n50df['tradingsymbol'])
